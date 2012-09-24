2bb8cb01548d5bdae65a6a85a23ce423495f04bb
Jishnu Mohan <mohanjishnu@gmail.com>
2012-09-24 11:20:47 +0800

    I'm a jQuery ninja

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 //$("img").width(200);
 $(document).ready(function(){
 
+	$("li").on("click",function(){
+			if($(this).hasClass("done")){
+				$(this).removeClass("done");
+			} else {
+				$(this).addClass("done");
+			}
+			//$("li").addClass("done");
+		});
+
+
 	$("img").on("dblclick",function(){
 
 		$("img").width(200);
